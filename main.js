e9991136af93696c3992429fe371b3e19439fe29
Saravana Kumar V <99715784+Saravanakumar-v@users.noreply.github.com>
2022-05-02 14:33:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 
 function getWeatherDetails(id){
     var cityName = document.getElementById(id).value;
-    alert(cityName);
-
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
-            'X-RapidAPI-Key': 'ffdeab80a5msh9dd3cac123a56aep13d49fjsnb8d94918ecc9'
-        }
-    };
     
-    fetch('https://weatherapi-com.p.rapidapi.com/ip.json?q= cityName', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+    const data = null;
+
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("GET", "https://community-open-weather-map.p.rapidapi.com/weather?q=London%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=imperial&mode=xml");
+    xhr.setRequestHeader("X-RapidAPI-Host", "community-open-weather-map.p.rapidapi.com");
+    xhr.setRequestHeader("X-RapidAPI-Key", "c3d1c7f152msh080f77e9063ce2bp1fbc12jsn8352894f719c");
+
+    xhr.send(data);
 
 }
